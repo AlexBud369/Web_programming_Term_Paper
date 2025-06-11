691aa89fcc3d7e696ce155bc90d6432a921a9a96
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-06-11 11:34:10 +0300

    feat: add preloader module

---
+++ js/modules/preloader.js
@@ -0,0 +1,29 @@
+export function showPreloader() {
+    const preloader = document.querySelector('.preloader');
+    if (preloader) {
+        preloader.style.display = 'flex';
+    }
+}
+
+export function hidePreloader() {
+    const preloader = document.querySelector('.preloader');
+    if (preloader) {
+        preloader.style.opacity = '0';
+        setTimeout(() => {
+            preloader.style.display = 'none';
+        }, 300); 
+    }
+}
+
+export function initPreloader() {
+    const preloader = document.querySelector('.preloader');
+    if (preloader) {
+        showPreloader();
+        window.addEventListener('load', () => {
+            hidePreloader();
+        });
+
+        document.addEventListener('asyncStart', () => showPreloader());
+        document.addEventListener('asyncEnd', () => hidePreloader());
+    }
+}
